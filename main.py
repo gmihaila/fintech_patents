9192f828d3cf95c62ab0486897c8adea340ae8b7
georgemihaila <georgemihaila20@yahoo.com>
2020-10-28 22:03:00 -0500

    files for test demo

---
+++ main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+# streamlit run main.py
+
+st.title('Patent classification with Transformers')
+
+st.subheader('Make predictions')
+
+st.write('More details go here...')
+
+
+# @st.cache(suppress_st_warning=True)
+def custom(name):
+
+    st.selectbox('Choose Model', ['Bert', 'RoBerTa'])
+
+    default_patent = 'This is a patent'
+
+    user_input = st.text_area("Patent Text Goes Here:", default_patent)
+
+    if st.button('Get Prediction!'):
+        st.text('Finetech')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    custom('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
